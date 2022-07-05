cc8b7f64a770c1766a2aa5e57255384aec5f6e20
-Unknown- <mrunknownyt@gmail.com>
2022-07-04 18:47:47 -0600

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module ulibs.engine.server {
+	exports main.java.ulibs.engine.server;
+	
+	requires java.desktop;
+	requires com.google.gson;
+	requires ulibs.common;
+	requires ulibs.engine.common;
+}
